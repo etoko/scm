6a5a9dbf71c14093fc43fda7052b9004feb7ede2
Emmanuel Toko <emmanuel.toko@gmail.com>
2013-03-05 17:06:05 +0300

    Adding Administrator Account as first user

--- scripts/populate.py
+++ scripts/populate.py
@@ -11,7 +11,8 @@ from pyramid.paster import (
 
 from ..models import (
     DBSession,
-    MyModel,
+#    MyModel,
+    User,
     Base,
     )
 
@@ -31,5 +32,8 @@ def main(argv=sys.argv):
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
     with transaction.manager:
-        model = MyModel(name='one', value=1)
-        DBSession.add(model)
+ #       model = MyModel(name='one', value=1)
+  #      DBSession.add(model)
+        user = User("editor", "Emmanuel", "Toko")
+        user.is_superuser = False
+        DBSession.add(user)
