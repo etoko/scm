9431e743741e394e6cb7c03302af0cbe36a7f484
Emmanuel Toko <emmanuel.toko@gmail.com>
2013-03-21 10:22:55 +0300

    Created requisition Controller

---
+++ controllers/requisition.py
@@ -0,0 +1,16 @@
+
+class RequisitionController(ApiController):
+    """
+    Class manages requistion operations
+    """
+    def create(self, j_requisition):
+        pass
+
+    def update(self, j_requisition):
+        pass
+
+    def get(self, j_requisition):
+        pass
+
+    def delete(self, j_requisition):
+        pass
